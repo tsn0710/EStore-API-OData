e10190951fead1a8b9bbfe50cdf6ca1bc4aa4c56
tsn0710 <tongnhat07102001@gmail.com>
2023-10-12 09:22:29 +0700

    1
    chưa có code phức tạp, chỉ có tạo sẵn db và một controller odata

---
+++ EStore/StoreAPI/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.OData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OData.ModelBuilder;
+using StoreAPI.Models;
+
+namespace StoreAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var modelBuilder = new ODataConventionModelBuilder();
+            modelBuilder.EntitySet<Member>("Members");
+            modelBuilder.EntitySet<Category>("Categories");
+            modelBuilder.EntitySet<Product>("Products");
+            modelBuilder.EntitySet<Order>("Orders");
+            modelBuilder.EntitySet<OrderDetail>("OrderDetails");
+
+            builder.Services.AddDbContext<eStoreContext>();
+
+            builder.Services.AddControllers().AddOData(
+                options => options.Select().Filter().OrderBy().Expand().Count().SetMaxTop(null).AddRouteComponents(
+                    "odata",
+                    modelBuilder.GetEdmModel()));
+
+
+
+            var app = builder.Build();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+            app.Run();
+        }
+    }
+}
---
+++ EStore/StoreClient/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StoreClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
